c19acbb0b3e39afebcedf70f2edc99655f3c3dfc
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2025-05-15 07:53:30 -0700

    chore: update dependencies and configuration for Tailwind CSS
    - Updated Next.js version to 14.2.28 in package.json.
    - Removed redundant Next.js entry in dependencies.
    - Added @tailwindcss/postcss as a dependency.
    - Updated postcss.config.mjs to use '@tailwindcss/postcss' plugin.
    - Removed tailwind.config.ts file as it is no longer needed.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center font-[family-name:var(--font-geist-sans)]">
+		<div className="flex flex-col items-center font-(family-name:--font-geist-sans)">
 			fff
 		</div>
 	);
